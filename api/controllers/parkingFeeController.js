bbe4f7c48ea0eaa3905f30ec33723e6a0281b1c0
Billy P. Witt <billy.coco6@gmail.com>
2017-05-20 10:06:35 +0700

    reading record

--- api/controllers/parkingFeeController.js
+++ api/controllers/parkingFeeController.js
@@ -4,6 +4,19 @@ var mongoose = require('mongoose'),
 	Schema = mongoose.Schema;
 
 exports.test = function(req, res) {
+	mongoose.model('parkingfees',
+          new mongoose.Schema({
+            mall_id: Number,
+            mall_name: String,
+            rounding_time: Number,
+            fee_period: Number,
+            long_hour_fee: Number,
+            rate: [{
+              duration: Number,
+              cost: Number
+            }]
+          }));
+	
 	var parkingfees = mongoose.model('parkingfees');
 	parkingfees.find({}, function(err, data) { 
 		res.json(err, data, data.length); 
